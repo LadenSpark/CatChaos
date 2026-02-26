6c2e74bf7f05d5f8fcb63c6cf4d8e126c119f037
LadenSpark <ladenspark@outlook.com>
2026-02-26 15:48:33 -0800

    v1.48.1a
    Change Log:
    - Added separate single player system logic.
    - Added a cute animated dog (it is not ours, lets fix that)
    - Code is a little clunky rn... I will fix asap

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.SceneManagement;
 
-public class GameManager : NetworkBehaviour
+public class GameManager : MonoBehaviour
 {
     public static GameManager _instance;
 
-    [Header("Prefabs")]
+    // [Header("Prefabs")]
     [SerializeField] private GameObject singlePlayerPrefab;
-    [SerializeField] private GameObject networkPlayerPrefab;
+    // [SerializeField] private GameObject networkPlayerPrefab;
 
-    [Header("Spawn Settings")]
+    // [Header("Spawn Settings")]
     [SerializeField] private Transform spawnPoint;
-    [SerializeField] private Transform playerTwoSpawnPoint;
+    // [SerializeField] private Transform playerTwoSpawnPoint;
 
     private void Awake()
     {
@@ -25,51 +25,8 @@ public class GameManager : NetworkBehaviour
         {
             _instance = this;
         }
+
+        Instantiate(singlePlayerPrefab, spawnPoint.transform.position, Quaternion.identity);
     }
 
-    // --- Mode Starters ---
-
-    /*public void StartSinglePlayer()
-    {
-        Debug.Log("Starting Single Player Mode...");
-        // Disable NetworkManager to avoid interference if necessary
-        //if (NetworkManager.Singleton != null) NetworkManager.Singleton.gameObject.SetActive(false);
-
-        // Standard Unity Instantation
-        Instantiate(singlePlayerPrefab, spawnPoint.position, Quaternion.identity);
-    }
-
-    public void StartNetworkHost()
-    {
-        Debug.Log("Starting Network Host (Server + Player)...");
-        ConfigureNetworkPrefab();
-        NetworkManager.Singleton.StartHost();
-    }
-
-    public void StartNetworkClient()
-    {
-        Debug.Log("Starting Network Client...");
-        // Prefab config is usually handled by the Host, but we set it for safety
-        ConfigureNetworkPrefab();
-        NetworkManager.Singleton.StartClient();
-    }
-
-    private void ConfigureNetworkPrefab()
-    {
-        if (NetworkManager.Singleton != null)
-        {
-            // Update the NetworkManager's player prefab slot at runtime
-            var transport = NetworkManager.Singleton.gameObject.GetComponent<NetworkConfig>();
-            NetworkManager.Singleton.NetworkConfig.PlayerPrefab = networkPlayerPrefab;
-        }
-    }
-
-    public void ResetGame()
-    {
-        if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsListening)
-        {
-            NetworkManager.Singleton.Shutdown();
-        }
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }*/
 }
