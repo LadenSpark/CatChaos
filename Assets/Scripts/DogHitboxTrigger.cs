4613d16566b5b0e4acd9103d171a3db1b994d450
TopherTV <topher.anderson7@gmail.com>
2026-05-14 11:16:59 -0700

    dog and cat midprogress
    reorganized files, modified dog and cat to have new animations, resized sprites, added anim triggers. need to finalize cat anims and add attack trigger for cat.

--- Assets/Scripts/DogHitboxTrigger.cs
+++ Assets/Scripts/DogHitboxTrigger.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 
 public class DogHitboxTrigger : MonoBehaviour
 {
+
+    private Animator dogAnim;
+
+    void Awake()
+    {
+        dogAnim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Dog hitbox triggered by: " + other.gameObject.name);
-        if (other.CompareTag("FallingObject")) ;
+        if (other.CompareTag("FallingObject"))
         {
             // Onomatopoeia text graphic for a dog getting hit by a falling object
             Debug.Log("Dog hit by a falling object!");
             Instantiate(Resources.Load("BonkText"), transform.position, Quaternion.identity);
+
+
+            dogAnim.SetTrigger("dogHit");
+            dogAnim.ResetTrigger("dogHit");
 
             //Add score with scoremanager, NEED TO CREATE
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,6 +33,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool isFacingRight = true; 
 
     private Rigidbody2D rb;
+    private Animator catAnim;
     private bool isGrounded;
     private bool isHanging = false;
     private bool hasAttemptedHang = false; 
@@ -45,6 +46,7 @@ public class PlayerController : MonoBehaviour
 
     void Awake() 
     {
+        catAnim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true; 
         rb.interpolation = RigidbodyInterpolation2D.Interpolate;
@@ -76,9 +78,19 @@ public class PlayerController : MonoBehaviour
         // 2. Fallback to Keyboard if Joystick is idle
         if (Mathf.Abs(moveInput) < 0.01f)
         {
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) moveInput = -1;
-            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) moveInput = 1;
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                catAnim.SetBool("walk", true);
+                moveInput = -1;
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                catAnim.SetBool("walk", true);
+                moveInput = 1;
+            }
+            else catAnim.SetBool("walk", false);
         }
+        else catAnim.SetBool("walk", true);
 
         // 3. Flip Logic (Ass-Backward Fix)
         if (moveInput > 0.1f && !isFacingRight) Flip();
@@ -88,12 +100,17 @@ public class PlayerController : MonoBehaviour
 
         // 4. Ground Check (Multi-Layer)
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-        if (isGrounded) hasAttemptedHang = false;
+        if (isGrounded)
+        {
+            catAnim.SetBool("Catjump", false);
+            hasAttemptedHang = false;
+        }
+        else catAnim.SetBool("Catjump", true);
 
-        // 5. Jump (Velocity reset for height consistency)
-        bool jumpPressed = Input.GetKeyDown(KeyCode.Space) || 
-                           Input.GetKeyDown(KeyCode.W) || 
-                           TCKInput.GetButtonDown(jumpButtonIdentifier);
+            // 5. Jump (Velocity reset for height consistency)
+            bool jumpPressed = Input.GetKeyDown(KeyCode.Space) ||
+                               Input.GetKeyDown(KeyCode.W) ||
+                               TCKInput.GetButtonDown(jumpButtonIdentifier);
 
         if (jumpPressed && isGrounded)
         {
