3f302dbbeff438bfb81a669cdf184738eb924882
TopherTV <topher.anderson7@gmail.com>
2026-05-21 17:33:40 -0700

    dogBark
    grabbed an alternate hit animation for the dog and used it to take the place of the dog barking. set the code to make the dog freeze in place every 5 or so seconds and play the anim. dog no longer slides when barking or hit. cat still slides while swiping - might want to leave it that way so the player can't use the swipe as a free emergency brake button.

--- Assets/Scripts/DogHitboxTrigger.cs
+++ Assets/Scripts/DogHitboxTrigger.cs
@@ -6,15 +6,37 @@ using UnityEngine;
 public class DogHitboxTrigger : MonoBehaviour
 {
     private Animator dogAnim;
-
+    private int frameselapsed;
     void Awake()
     {
+        frameselapsed = 0;
         dogAnim = GetComponent<Animator>(); //need this for the dog hit animation
         dogAnim.SetBool("dogHit", false);
+        dogAnim.SetBool("dogBark", false);
     }
+
+    void FixedUpdate()
+    {
+        DogBarkTimer();
+        
+        
+    }
+
+    private void DogBarkTimer()
+    {
+        if (frameselapsed > 300)
+        {
+            dogAnim.SetBool("dogBark", true);
+            frameselapsed = 0;
+        }
+        else frameselapsed++;
+
+    }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        dogAnim.SetBool("dogBark", false);
         Debug.Log("Dog hitbox triggered by: " + other.gameObject.name);
         if (other.CompareTag("FallingObject"))
         {
@@ -26,6 +48,11 @@ public class DogHitboxTrigger : MonoBehaviour
             dogAnim.SetBool("dogHit", true);
             //Add score with scoremanager, NEED TO CREATE
         }
+    }
+
+    private void DogBarkConclusion()
+    {
+        dogAnim.SetBool("dogBark", false);
     }
 
     private void DogHitConclusion() //called by the animation when the swipe ends, necessary to lock in place for the duration of the swipe
